d95a0ed2093c29dffc5e7fde977e608a57ab3c38
Shah Z. S <shahzshafie@outlook.com>
2020-08-10 13:15:15 +0800

    starter app for server

---
+++ middle-man/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace middle_man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
